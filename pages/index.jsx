e6705b093ddade8711d88be7eba32309a11cc9f5
Nilton Rocha <nilton.silva@grupocard.com.br>
2022-12-21 01:18:53 -0300

    atualização login form input v2

--- pages/index.jsx
+++ pages/index.jsx
@@ -16,45 +16,37 @@ export default function Home() {
 
   const router = useRouter()
 
-  useEffect(() => {
-    if(window.localStorage.user) {
-      setUser(window.localStorage.user)
-    } else {
-      router.push('/login')
-    }
-  }, [])
-
   if(!loading) {
 
-    fetch('https://www.vtal.com.br/portal-do-cliente/retirada-de-circuito', {
-      headers: {
-        // 'Access-Control-Allow-Origin': '*',
-        // 'Content-Type': 'text/html; charset=iso-8859-1',
-        // 'Location': 'http://www.vtal.com.br/portal-do-cliente/retirada-de-circuito/',
-        // 'User-Agent': 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Mobile Safari/537.36',
-        // 'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-        // 'DNT': 1,
-        // 'Upgrade-Insecure-Requests': 1  
+    // fetch('https://www.vtal.com.br/portal-do-cliente/retirada-de-circuito', {
+    //   headers: {
+    //     // 'Access-Control-Allow-Origin': '*',
+    //     // 'Content-Type': 'text/html; charset=iso-8859-1',
+    //     // 'Location': 'http://www.vtal.com.br/portal-do-cliente/retirada-de-circuito/',
+    //     // 'User-Agent': 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Mobile Safari/537.36',
+    //     // 'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+    //     // 'DNT': 1,
+    //     // 'Upgrade-Insecure-Requests': 1  
 
-        headers: {
-          'Content-Length': 270,
-          'Content-Security-Policy': 'frame-ancestors https://vtal.com.br https://www.vtal.com.br http://vtal.com.br http://www.vtal.com.br https://www.youtube.com/ https://youtube.com/ https://www.google.com/ https://google.com/ https://www.googletagmanager.com/ https://googletagmanager.com/;',
-          'Content-Type': 'text/html; charset=iso-8859-1',
-          'Date': 'Sat, 17 Dec 2022 03:17:57 GMT',
-          'Location': 'http://www.vtal.com.br/portal-do-cliente/retirada-de-circuito/',
-          'Server': 'Vtal',
-          'X-Frame-Options': 'SAMEORIGIN',
-          'Access-Control-Allow-Origin': '*',
-          'Location': 'http://www.vtal.com.br/portal-do-cliente/retirada-de-circuito/',
-          'User-Agent': 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Mobile Safari/537.36',
-          'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-        }
-      }
-    }).then((res) => {
-      console.log(res)
-    }).catch((error) => {
-      console.log(error)
-    })
+    //     headers: {
+    //       'Content-Length': 270,
+    //       'Content-Security-Policy': 'frame-ancestors https://vtal.com.br https://www.vtal.com.br http://vtal.com.br http://www.vtal.com.br https://www.youtube.com/ https://youtube.com/ https://www.google.com/ https://google.com/ https://www.googletagmanager.com/ https://googletagmanager.com/;',
+    //       'Content-Type': 'text/html; charset=iso-8859-1',
+    //       'Date': 'Sat, 17 Dec 2022 03:17:57 GMT',
+    //       'Location': 'http://www.vtal.com.br/portal-do-cliente/retirada-de-circuito/',
+    //       'Server': 'Vtal',
+    //       'X-Frame-Options': 'SAMEORIGIN',
+    //       'Access-Control-Allow-Origin': '*',
+    //       'Location': 'http://www.vtal.com.br/portal-do-cliente/retirada-de-circuito/',
+    //       'User-Agent': 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Mobile Safari/537.36',
+    //       'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+    //     }
+    //   }
+    // }).then((res) => {
+    //   console.log(res)
+    // }).catch((error) => {
+    //   console.log(error)
+    // })
 
 
     // const api = fetch(`/api/cnpj/${cnpj}`, {
